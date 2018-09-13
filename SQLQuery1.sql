04829c794c4c202dab9ce2ab7f62fe6560b81910
Jorge-Caceres <jorge97caceres@gmail.com>
2018-09-12 20:08:23 -0400

    todos los prodecimientos creados

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,9 +1,9 @@
 --PROCEDIMIENTO DE ALMACENADO PARA EL PROYECTO
 --se usa ALTER para hacer la modificacion
-create procedure spConsultarCiudades
+ALTER procedure spConsultarCiudades
 as
 begin
-	select * from Ciudad order by Descripcion
+	select * from Ciudad order by CiudadID asc
 end
 
 --en el sistema se usara solo el llamado a un procedimiento
@@ -17,3 +17,34 @@ as
 begin
 	select * from Ciudad where CiudadID = @CodCiudad
 end
+
+
+--insercion en la tabla con el procedimiento de almacenado
+CREATE PROCEDURE spInsertarCiudad @descripcion varchar(50)
+AS
+BEGIN
+	INSERT INTO Ciudad(Descripcion) VALUES(@descripcion)
+END
+
+EXEC spInsertarCiudad 'San Antonio'
+
+delete from Ciudad where CiudadID = 5
+
+CREATE PROCEDURE spActualizarCiudad @codigo int, @descripcion varchar(55)
+AS
+BEGIN
+	update Ciudad
+		set Descripcion = @descripcion
+		where CiudadID = @codigo
+END
+
+exec spActualizarCiudad 6, 'Ñemby'
+
+CREATE PROCEDURE spEliminarCiudad @codigo int 
+AS
+BEGIN
+	DELETE FROM Ciudad 
+	WHERE CiudadID = @codigo
+END
+
+EXEC spEliminarCiudad 6
