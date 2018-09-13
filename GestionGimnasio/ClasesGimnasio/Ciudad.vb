f9008461ff23d0f07a5c18723a4692b1c7efefb2
Jorge-Caceres <jorge97caceres@gmail.com>
2018-09-12 21:15:06 -0400

    clase de ciudad

---
+++ GestionGimnasio/ClasesGimnasio/Ciudad.vb
@@ -0,0 +1,23 @@
+﻿Public Class Ciudad
+
+    Private CiudadId As Integer
+    Private Descripcion As String
+
+    Public Property CiudadId1 As Integer
+        Get
+            Return CiudadId
+        End Get
+        Set(value As Integer)
+            CiudadId = value
+        End Set
+    End Property
+
+    Public Property Descripcion1 As String
+        Get
+            Return Descripcion
+        End Get
+        Set(value As String)
+            Descripcion = value
+        End Set
+    End Property
+End Class
