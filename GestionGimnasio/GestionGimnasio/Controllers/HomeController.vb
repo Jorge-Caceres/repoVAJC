a20b193dab31ab5cc18a1301a495abd1f306d03f
Alumno 203 <alumno-203@uaa.edu.py>
2018-09-19 19:27:38 -0400

    se crea la vista para el controller que inicializa la conexion a BD

---
+++ GestionGimnasio/GestionGimnasio/Controllers/HomeController.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Web.Mvc
+Imports ClasesGimnasio
+Namespace Controllers
+    Public Class HomeController
+        Inherits Controller
+
+        ' GET: Home
+        Function Index() As ActionResult
+            ''se inicializa la conexion a la BD
+            Util.inicializaSesion("M203-13", "Gym", "sa", "@lumno123")
+
+            Return View()
+        End Function
+    End Class
+End Namespace
---
+++ GestionGimnasio/GestionGimnasio/Views/Home/Index.vbhtml
@@ -0,0 +1,16 @@
+﻿
+@Code
+    Layout = Nothing
+End Code
+
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta name="viewport" content="width=device-width" />
+    <title>Sistema de Gestion para GYM </title>
+</head>
+<body>
+    <h1>Sistema de gestion para gimnasio</h1>
+</body>
+</html>
